8013dc0e57dfac3d63a110a8ad6c863575684fda
Nahom d <nahom.d54@gmail.com>
2024-11-20 17:16:18 +0300

    Refactor bot implementation to use Telegraf and restructure middleware handling

--- bot.js
+++ bot.js
@@ -1,46 +1,54 @@
 require("dotenv").config();
-const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
-const commands = require("./commands");
+const { Telegraf } = require("telegraf");
+// const express = require("express");
+const router = require("./handlers/command");
+const cbrouter = require("./handlers/callback");
+
+const connectDB = require("./config/mongoose");
+const { userMiddleware } = require("./controllers/middlewares");
 
 const TOKEN = process.env.TG_BOT_TOKEN;
-const WEBHOOK_URL = process.env.WEBHOOK_URL;
-const PORT = process.env.PORT || 3000;
+// const WEBHOOK_URL = process.env.WEBHOOK_URL;
+// const PORT = process.env.PORT || 3000;
+// const DOMAIN = "google.com";
 
-// Initialize bot with webhook settings
-const bot = new TelegramBot(TOKEN, { webHook: true });
-bot.setWebHook(`${WEBHOOK_URL}/bot/tg-webhook`, {
-  secret_token: process.env.TG_SECRET_TOKEN,
-});
+connectDB();
+const bot = new Telegraf(TOKEN);
 
 // Initialize the Express server
-const app = express();
-app.use(express.json());
 
-// Define webhook route
-app.get("/", (req, res) => {
-  res.send({ status: "ok" });
+// const app = express();
+
+// app.use(express.json());
+// (async () => {
+//   app.use(
+//     await bot.createWebhook({
+//       url: DOMAIN,
+//       path: "/bot",
+//       secret_token: process.env.TG_SECRET_TOKEN,
+//     })
+//   );
+// })();
+
+bot.use(userMiddleware);
+bot.use(router);
+bot.use(cbrouter);
+bot.catch((err, ctx) => {
+  console.error(`Error for ${ctx.updateType}`, err);
+  ctx.reply("An error occurred");
 });
-app.post(`/bot/tg-webhook`, (req, res) => {
-  if (
-    req.headers["X-Telegram-Bot-Api-Secret-Token"] !==
-    process.env.TG_SECRET_TOKEN
-  ) {
-    return res.sendStatus(403);
-  }
-  bot.processUpdate(req.body); // Pass incoming request to bot
-  res.sendStatus(200);
-});
 
-// Load bot commands
-commands(bot);
-
-// Start the Express server
+// app.get("/", (req, res) => {
+//   res.status(200).json({ status: "ok" });
+// });
 
 if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Express server is running on port ${PORT}`);
+  bot.launch(() => {
+    console.log("Bot Running");
   });
+  // app.listen(PORT, () => {
+  //   console.log(`Express server is running on port ${PORT}`);
+  // });
 }
 
-module.exports = app;
+module.exports = { bot };
